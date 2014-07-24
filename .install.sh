aae12642b7b51d56ca309d41251263e4e9a136e2
clsdx <c.seydoux@gmx.com>
2014-07-24 09:43:47 +0000

    changes to symbolic links in install.sh

--- .install.sh
+++ .install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-cp -r ./.bash_profile ~/
-cp -r ./.gitconfig ~/
-cp -r ./.vim ~/
-cp -r ./.vimrc ~/
-cp -r ./.ssh ~/
+ln -s dotfiles/.bash_profile ~/
+ln -s dotfiles/.bashrc ~/
+ln -s dotfiles/.gitconfig ~/
+ln -s dotfiles/.git-prompt.sh ~/
+ln -s dotfiles/.ssh ~/
+ln -s dotfiles/.vim ~/
+ln -s dotfiles/.vimrc ~/
+~/.bash_profile
 source ~/.bashrc
 source ~/.vimrc
 echo "done"
