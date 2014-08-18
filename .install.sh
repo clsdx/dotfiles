90fc5d199d782e405f076d1b5e5453b6b30f2afd
Cyril Seydoux <c.seydoux@gmx.com>
2014-08-18 11:27:06 +0000

    new tmux kickass config

--- .install.sh
+++ .install.sh
@@ -5,6 +5,7 @@ ln -s dotfiles/.gitconfig ~/
 ln -s dotfiles/.git-prompt.sh ~/
 ln -s dotfiles/.vim ~/
 ln -s dotfiles/.vimrc ~/
+ln -s dotfiles/.tmux.conf ~/
 ~/.bash_profile
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 source ~/.bashrc
